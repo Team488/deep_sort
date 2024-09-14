449d5fd7acb325fa8c060640511bb4f755e2ba2e
Saqib Rokadia <saqib@saqibr.com>
2024-09-14 16:18:30 -0700

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import pathlib
 
-setup(name='deep_sort',
+import pkg_resources
+import setuptools
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+
+setuptools.setup(name='deep_sort',
       version='0.1',
       description='Python Distribution Utilities',
       author='Saqib Rokadia',
       author_email='saqib@saqibr.com',
       url='https://github.com/rokadias/deep_sort',
-      packages=['distutils', 'distutils.command'],
+      packages=['deep_sort'],
+      install_reqs=install_requires,
      )
