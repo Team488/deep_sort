1a6027efac5d42625df3aacfc73d2ce4f4022320
Saqib Rokadia <saqib@saqibr.com>
2024-09-14 14:07:52 -0700

    Adding setup.py for deep_sort.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='deep_sort',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Saqib Rokadia',
+      author_email='saqib@saqibr.com',
+      url='https://github.com/rokadias/deep_sort',
+      packages=['distutils', 'distutils.command'],
+     )
